0bfba1d5256fe219548ce3da937919831d76ccbd
Ahmad Malik <arma7x@live.com>
2023-07-15 22:29:06 +0800

    fix example

--- mock-react-native/app/screens/login.js
+++ mock-react-native/app/screens/login.js
@@ -1,17 +1,9 @@
-import {
-    StyleSheet,
-    Text,
-    View,
-    Dimensions,
-    TouchableOpacity,
-    Button,
-    TextInput,
-    FlatList,
-} from 'react-native';
+import { useState } from 'react';
+import { Text, View, TextInput, FlatList } from 'react-native';
 
 const DATA = [];
 
-for (let i=0;i<50;i++) {
+for (let i=1;i<=50;i++) {
     DATA.push({
         id: i.toString(),
         title: `Item ${i}`,
@@ -20,14 +12,21 @@ for (let i=0;i<50;i++) {
 
 export default function LoginScreen() {
 
-  return (
-      <View style={{ flex: 1 }}>
-        <FlatList
-            data={DATA}
-            renderItem={({item}) => <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}><Text>{item.title}</Text></View>}
-            keyExtractor={item => item.id}
-        />
-        <TextInput value={"1234"}/>
-      </View>
-  );
+    const [value, setValue] = useState("");
+
+    return (
+        <View style={{ flex: 1 }}>
+            <FlatList
+                data={DATA}
+                renderItem={({item}) => <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}><Text>{item.title}</Text></View>}
+                keyExtractor={item => item.id}
+            />
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <Text>ENTER: </Text>
+                <View style={{ flex: 1 }}>
+                    <TextInput value={value} style={{ backgroundColor: '#c0c0c0', padding: 5 }} onChangeText={setValue} placeholder="Enter any value" />
+                </View>
+            </View>
+        </View>
+    );
 }
